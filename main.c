419d7fdd0a164b5278a9660a10ea623500023606
siara989 <siara.roxanna@gmail.com>
2018-11-11 04:27:50 +0530

    add a program written in c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int no1,no2,sum;
+    printf("Enter first number:\n");
+    scanf("%d",&no1);
+    printf("Enter second number:\n");
+    scanf("%d",&no2);
+    sum=no1+no2;
+    printf("The sum of two numbers is:\t\n%d",sum);
+    return 0;
+}
